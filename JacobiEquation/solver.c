4c5a2f0a9eb9348a0cb08a59c19d0ce2620012b7
ShipperNewt <49525989+ShipperNewt@users.noreply.github.com>
2019-04-22 13:36:41 -0400

    Update solver.c
    Started Pthread implementation

--- JacobiEquation/solver.c
+++ JacobiEquation/solver.c
@@ -27,6 +27,18 @@ void print_grid (grid_t *);
 void print_stats (grid_t *);
 double grid_mse (grid_t *, grid_t *);
 
+typedef struct thread_data_s
+{
+	int tid;
+	unsigned int chunk_size;
+	unsigned int num_elements;
+	int num_threads;
+	float* grid;
+	int i, j;
+	double diff;
+	float old, new; 
+	int num_iter = 0;
+} thread_data_t;
 
 int 
 main (int argc, char **argv)
@@ -86,12 +98,90 @@ main (int argc, char **argv)
 
 /* FIXME: Edit this function to use the jacobi method of solving the equation. The final result should be placed in the grid data structure. */
 int 
-compute_using_pthreads_jacobi (grid_t *grid, int num_threads)
+compute_using_pthreads_jacobi (grid_t *grid, int num_threads, unsigned int num_elements)
 {		
-    return 1;
+    int i, j;
+	
+	pthread_t* thread_id = (pthread_t *) malloc(sizeof(pthread_t) * num_threads); //This allocates the memory needed for the total amount of threads
+	thread_data_t *thread_data_array = (thread_data_t *) malloc(sizeof(thread_data_t) * num_threads);
+	pthread_attr_t attributes; //
+	pthread_attr_init (&attributes);
+	//int chunk_size = (int) floor(MATRIX_SIZE / num_threads); //chunk_size is how many rows to do
+	barrier.counter = 0;
+        sem_init (&barrier.counter_sem, 0, 1); /* Initialize the semaphore protecting the counter to unlocked. */
+	sem_init (&barrier.barrier_sem, 0, 0); /* Initialize the semaphore protecting the barrier to locked. */
+	//sem_init(&sem, 0, 0);
+
+	for(i = 0; i < num_threads; i++)
+	{
+		thread_data_array[i].num_iter = num_iter;
+		thread_data_array[i].diff = diff;
+		thread_data_array[i].old = old;
+		thread_data_array[i].new = new;
+		thread_data_array[i].tid = i;
+		thread_data_array[i].num_threads = num_threads;
+	} 
+	for(i = 0; i < num_threads; i++)
+	{
+		pthread_create(&thread_id[i], &attributes, jacobi, (void *) &thread_data_array[i]);
+	}
+	for (i = 0; i < num_threads; i++)
+	{
+        	pthread_join (thread_id[i], NULL);
+	}
+	free((void *) thread_data_array);
+	free((void *) thread_id);
 }
 
+void* jacobi(void* args)
+{
+	thread_data_t *thread_data = (thread_data_t *) args;
+	int tid = thread_data->tid;
+	int num_elements = thread_data->num_elements;
+	float* grid = thread_data->grid;
+	k = thread_data->k;
+	
+	int num_iter = 0;
+	int done = 0;
+    int i, j;
+	double diff;
+	float old, new; 
+    float eps = 1e-2; /* Convergence criteria. */
+    int num_elements; 
+	
+	while(!done) { /* While we have not converged yet. */
+        diff = 0.0;
+        num_elements = 0;
 
+        for (i = 1; i < (grid->dim - 1); i++) {
+            for (j = 1; j < (grid->dim - 1); j++) {
+                old = grid->element[i * grid->dim + j]; /* Store old value of grid point. */
+                /* Apply the update rule. */	
+                new = 0.25 * (grid->element[(i - 1) * grid->dim + j] +\
+                              grid->element[(i + 1) * grid->dim + j] +\
+                              grid->element[i * grid->dim + (j + 1)] +\
+                              grid->element[i * grid->dim + (j - 1)]);
+
+                grid->element[i * grid->dim + j] = new; /* Update the grid-point value. */
+                diff = diff + fabs(new - old); /* Calculate the difference in values. */
+                num_elements++;
+            }
+        }
+		
+        /* End of an iteration. Check for convergence. */
+        diff = diff/num_elements;
+        printf ("Iteration %d. DIFF: %f.\n", num_iter, diff);
+        num_iter++;
+			  
+        if (diff < eps) 
+            done = 1;
+	}
+	pthread_exit (NULL);
+    //return num_iter;
+	
+	
+	
+}
 
 /* Create a grid with the specified initial conditions. */
 grid_t * 
@@ -207,7 +297,24 @@ grid_mse (grid_t *grid_1, grid_t *grid_2)
     return mse/num_elem; 
 }
 
+void 
+barrier_sync(barrier_t *barrier, int tid, int num_threads)
+{
+    sem_wait (&(barrier->counter_sem));
 
-
-		
-
+    /* Check if all threads before us, that is num_threads - 1 threads have reached this point. */	  
+    if (barrier->counter == (num_threads - 1)) {
+        barrier->counter = 0; /* Reset the counter. */
+        sem_post (&(barrier->counter_sem)); 
+					 
+        /* Signal the blocked threads that it is now safe to cross the barrier. */
+        //printf ("Thread number %d is signalling other threads to proceed\n", tid); 
+        for (int i = 0; i < (num_threads - 1); i++)
+            sem_post (&(barrier->barrier_sem));
+    } 
+    else {
+        barrier->counter++;
+        sem_post (&(barrier->counter_sem));
+        sem_wait (&(barrier->barrier_sem)); /* Block on the barrier semaphore. */
+    }
+}
